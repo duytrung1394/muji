d6a5e132c237f046a836750e08a1d9830d06a563
Satoshi SUZUKI <akn@s-satoshi.net>
2018-06-18 15:34:47 +0900

    editの処理を共通化

---
+++ frontend/src/customApp/containers/shared/edit.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import {withRouter, Link} from 'react-router-dom';
+import {connect} from "react-redux";
+import TaskForm from './form';
+import LayoutWrapper from "../../../components/utility/layoutWrapper";
+import PageHeader from "../../../components/utility/pageHeader";
+import Box from '../../../components/utility/box';
+import Spin from '../../../components/uielements/spin';
+import Button from '../../../components/uielements/button';
+import actions from "../../redux/task/entity/actions";
+import {message} from "antd/lib/index";
+
+export default class RestEdit extends Component {
+  componentDidMount(){
+    // showから来たときはデータが既にある。 そうじゃない時はないのでロードが必要。
+    const {id} =  this.props.match.params;
+    if( this.props.entity.id !== parseInt(id) ){
+      // 読まれてるデータがURLと一致しない時はリロードする
+      this.props.fetchCleanup();
+      this.props.fetchRequest(id);
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, prevContext){
+    const {
+      name,
+      baseUrl,
+      updated,
+      entity,
+      updateCleanup,
+      history
+    } = this.props;
+
+    if(updated){
+      message.success(`${name}を更新しました`);
+      const id = entity.id;
+      updateCleanup();
+      history.push(`${baseUrl}/${id}`);
+    }
+  }
+
+  render() {
+    const {
+      name,
+      baseUrl,
+      formComponent,
+      // react-router
+      match,
+      // types
+      fetching,
+      fetched,
+      entity,
+      updating,
+      updated,
+      updateError,
+      updateErrors,
+      // methods
+      updateRequest,
+    } = this.props;
+
+    const {
+      id
+    } = match.params;
+
+    return (
+      <LayoutWrapper>
+        <PageHeader>
+          {name}の編集
+        </PageHeader>
+        <Link to={`${baseUrl}/${entity.id}`}>
+          <Button>
+            {name}詳細にもどる
+          </Button>
+        </Link>
+        <Box>
+          <Spin spinning={fetching || updating}>
+            { (() => {
+              if( fetched && !updated ){
+                return React.createElement(formComponent,{
+                  entity: entity,
+                  onSubmit: entity => updateRequest(id, entity),
+                  errors: updateErrors.task,
+                });
+              }
+            })() }
+          </Spin>
+        </Box>
+      </LayoutWrapper>
+    );
+  }
+}
