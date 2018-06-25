c8439a0667412087609dd9e5c6e36453e086d0ff
Satoshi SUZUKI <akn@s-satoshi.net>
2018-06-25 20:21:47 +0900

    idTokenを簡単に永続化

--- frontend/src/redux/auth/reducer.js
+++ frontend/src/redux/auth/reducer.js
@@ -1,19 +1,25 @@
 import { Map } from 'immutable';
 import actions from './actions';
 
+const localStorageKey = 'idToken';
+
 const initState = new Map({
-  idToken: null,
+  idToken: localStorage.getItem(localStorageKey),
   isError: false
 });
 
 export default function authReducer(state = initState, action) {
   switch (action.type) {
-    case actions.LOGIN_SUCCESS:
+    case actions.LOGIN_SUCCESS:{
+      localStorage.setItem(localStorageKey, action.token);
       return initState.set('idToken', action.token);
+    }
     case actions.LOGIN_ERROR:
       return initState.set('isError', true);
-    case actions.LOGOUT:
+    case actions.LOGOUT:{
+      localStorage.removeItem(localStorageKey);
       return initState;
+    }
     default:
       return state;
   }
