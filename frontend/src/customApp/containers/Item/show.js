ee34fecd1276f43ec60f0ca667fb74f8a9f875b1
kueba <koji.ueba@xenophy.com>
2018-06-27 15:12:23 +0900

    商品管理: 詳細にSKU表示

--- frontend/src/customApp/containers/Item/show.js
+++ frontend/src/customApp/containers/Item/show.js
@@ -3,6 +3,9 @@ import {connect} from 'react-redux';
 import RestShow from '../shared/show';
 import actions from '../../redux/item/entity/actions';
 
+import Box from '../../../components/utility/box';
+import BoxTitle from '../../../components/utility/boxTitle';
+
 class ShowEntity extends Component {
 
   render() {
@@ -17,34 +20,56 @@ class ShowEntity extends Component {
         baseUrl="/dashboard/items"
         {...this.props}
       >
-        <ul>
-          <li>JANコード: {entity.item_code}</li>
-          <li>商品番号: {entity.item_code_alt}</li>
-          <li>公開・非公開: {entity.status}</li>
-          <li>有効期限(FROM): {entity.start_timestamp}</li>
-          <li>有効期限(TO): {entity.end_timestamp}</li>
-          <li>商品名: {entity.item_name}</li>
-          <li>メモ: {entity.memo}</li>
-          <li>商品ラベル: {entity.item_label}</li>
-          <li>概要: {entity.description}</li>
-          <li>予約フラグ: {entity.reservation_flag}</li>
-          <li>店頭受取フラグ: {entity.store_receive_flag}</li>
-          <li>ブランドコード: {entity.brand_code}</li>
-          <li>ソート順: {entity.priority}</li>
-          <li>プロモーションステータス: {entity.promotion_status}</li>
-          <li>仕様情報: {entity.spec_info}</li>
-          <li>サイズ情報: {entity.size_info}</li>
-          <li>リンクURL: {entity.link_urls}</li>
-          <li>商品属性: {entity.attributes}</li>
-          <li>キーワード: {entity.keywords}</li>
-          <li>タグ: {entity.tags}</li>
-          <li>アクセスポリシー: {entity.access_policy}</li>
-          <li>ブランチ情報: {entity.branches}</li>
-          <li>バッヂ: {entity.badges}</li>
-          
-          <li>AddonServiceCodes: {entity.addon_service_codes}</li>
-          <li>Lang: {entity.lang}</li>
-        </ul>
+        <div>
+          <BoxTitle title="アイテム情報" />
+          <Box>
+            <ul>
+              <li>JANコード: {entity.item_code}</li>
+              <li>商品番号: {entity.item_code_alt}</li>
+              <li>公開・非公開: {entity.status}</li>
+              <li>有効期限(FROM): {entity.start_timestamp}</li>
+              <li>有効期限(TO): {entity.end_timestamp}</li>
+              <li>商品名: {entity.item_name}</li>
+              <li>メモ: {entity.memo}</li>
+              <li>商品ラベル: {entity.item_label}</li>
+              <li>概要: {entity.description}</li>
+              <li>予約フラグ: {entity.reservation_flag}</li>
+              <li>店頭受取フラグ: {entity.store_receive_flag}</li>
+              <li>ブランドコード: {entity.brand_code}</li>
+              <li>ソート順: {entity.priority}</li>
+              <li>プロモーションステータス: {entity.promotion_status}</li>
+              <li>仕様情報: {entity.spec_info}</li>
+              <li>サイズ情報: {entity.size_info}</li>
+              <li>リンクURL: {entity.link_urls}</li>
+              <li>商品属性: {entity.attributes}</li>
+              <li>キーワード: {entity.keywords}</li>
+              <li>タグ: {entity.tags}</li>
+              <li>アクセスポリシー: {entity.access_policy}</li>
+              <li>ブランチ情報: {entity.branches}</li>
+              <li>バッヂ: {entity.badges}</li>
+            </ul>
+          </Box>
+          <BoxTitle title="SKU" />
+          {(entity.sku ? entity.sku : []).map(sku => {
+            return (
+              <Box>
+                <ul>
+                  <li>sku_code: {sku.sku_code}</li>
+                  <li>sales_start_timestamp: {sku.sales_start_timestamp}</li>
+                  <li>sales_end_timestamp: {sku.sales_end_timestamp}</li>
+                  <li>status: {sku.status}</li>
+                  <li>start_timestamp: {sku.start_timestamp}</li>
+                  <li>end_timestamp: {sku.end_timestamp}</li>
+                  <li>item_code: {sku.item_code}</li>
+                  <li>sku_name: {sku.sku_name}</li>
+                  <li>delivery_type: {sku.delivery_type}</li>
+                  <li>msrp: {sku.msrp}</li>
+                  <li>currency_code: {sku.currency_code}</li>
+                </ul>
+              </Box>
+            );
+          })}
+        </div>
       </RestShow>
     );
   }
