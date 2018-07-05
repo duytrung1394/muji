c9a8c8dd2f871c8ca20a7f8b963ebaab21919a95
ITO Kiyoharu <haruit@gmail.com>
2018-07-05 13:51:13 +0900

    [update] ユーザ管理をジェネレータで再生成

--- frontend/src/customApp/redux/user/list/saga.js
+++ frontend/src/customApp/redux/user/list/saga.js
@@ -2,7 +2,7 @@ import actions from './actions';
 import {RESTListApi} from '../../../../restApi';
 import restAllSaga from '../../shared/list/saga_generator';
 
-const api = RESTListApi('users');
+const api = RESTListApi('users', 'codes');
 
 export default function* saga() {
   yield restAllSaga(api, actions)
--- frontend/src/customApp/sidebar.js
+++ frontend/src/customApp/sidebar.js
@@ -1,11 +1,6 @@
 // 注：本ファイルを編集するとジェネレータの挙動が壊れるかもしれないので、かならず動作確認してください
 const options = [
   {
-    key: "users",
-    label: "sidebar.user",
-    leftIcon: "ion-android-checkbox-outline"
-  },
-  {
     key: "categories",
     label: "sidebar.category",
     leftIcon: "ion-android-checkbox-outline"
@@ -18,6 +13,11 @@ const options = [
   {
     key: "tasks",
     label: "task.sidebar",
+    leftIcon: "ion-android-checkbox-outline"
+  },
+  {
+    key: "users",
+    label: "user.sidebar",
     leftIcon: "ion-android-checkbox-outline"
   }
 ];
