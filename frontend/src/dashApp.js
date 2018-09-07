ff31a378cae6d317105b7413a2fbf5b64892954f
Satoshi SUZUKI <akn@s-satoshi.net>
2018-09-07 16:35:20 +0900

    dashAppの画面に関わるhocを分離

--- frontend/src/dashApp.js
+++ frontend/src/dashApp.js
@@ -1,41 +1,20 @@
 import React from "react";
-import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
-import { store, history } from "./redux/store";
+import { history } from "./redux/store";
 import Routes from "./routes";
-import { ThemeProvider } from "styled-components";
-import { LocaleProvider } from "antd";
-import { IntlProvider } from "react-intl";
-import themes from "./settings/themes";
 import AppLocale from "./languageProvider";
-import config, {
-  getCurrentLanguage
-} from "./containers/LanguageSwitcher/config";
-import { themeConfig } from "./settings";
-import DashAppHolder from "./dashAppStyle";
 import Boot from "./redux/boot";
 
-const currentAppLocale =
-  AppLocale[getCurrentLanguage(config.defaultLanguage || "english").locale];
+import Hoc from './hoc';
 
 const DashApp = () => (
-  <LocaleProvider locale={currentAppLocale.antd}>
-    <IntlProvider
-      locale={currentAppLocale.locale}
-      messages={currentAppLocale.messages}
-    >
-      <ThemeProvider theme={themes[themeConfig.theme]}>
-        <DashAppHolder>
-          <Provider store={store}>
-            <BrowserRouter>
-              <Routes history={history} />
-            </BrowserRouter>
-          </Provider>
-        </DashAppHolder>
-      </ThemeProvider>
-    </IntlProvider>
-  </LocaleProvider>
+  <Hoc>
+    <BrowserRouter>
+      <Routes history={history} />
+    </BrowserRouter>
+  </Hoc>
 );
+
 Boot()
   .then(() => DashApp())
   .catch(error => console.error(error));
---
+++ frontend/src/hoc.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { ThemeProvider } from "styled-components";
+import { LocaleProvider } from "antd";
+import { IntlProvider } from "react-intl";
+import themes from "./settings/themes";
+import AppLocale from "./languageProvider";
+import config, {
+  getCurrentLanguage
+} from "./containers/LanguageSwitcher/config";
+import { themeConfig } from "./settings";
+import DashAppHolder from "./dashAppStyle";
+
+const currentAppLocale =
+  AppLocale[getCurrentLanguage(config.defaultLanguage || "english").locale];
+
+const Hoc = (props) => (
+  <LocaleProvider locale={currentAppLocale.antd}>
+    <IntlProvider
+      locale={currentAppLocale.locale}
+      messages={currentAppLocale.messages}
+    >
+      <ThemeProvider theme={themes[themeConfig.theme]}>
+        <DashAppHolder>
+          <Provider store={store}>
+            {props.children}
+          </Provider>
+        </DashAppHolder>
+      </ThemeProvider>
+    </IntlProvider>
+  </LocaleProvider>
+
+);
+
+export default Hoc;
+export { AppLocale };
