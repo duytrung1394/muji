958eeabe5f1de7bfe7b7ae327420d14ddd33a295
Satoshi SUZUKI <akn@s-satoshi.net>
2018-09-07 15:13:51 +0900

    コメント削除

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -23,26 +23,7 @@ if (process.env.NODE_ENV !== 'production') {
     debug: true,
   }));
 }
-// app.use(express.static(path.join(__dirname, '../build')));
 
 app.use(router);
-//app.use('/about', router);
 
 app.listen(3000);
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import DashApp from "./dashApp";
-// import registerServiceWorker from "./registerServiceWorker";
-// import "antd/dist/antd.css";
-
-// ReactDOM.render(<DashApp />, document.getElementById("root"));
-
-// // Hot Module Replacement API
-// if (module.hot) {
-//   module.hot.accept("./dashApp.js", () => {
-//     const NextApp = require("./dashApp").default;
-//     ReactDOM.render(<NextApp />, document.getElementById("root"));
-//   });
-// }
-// registerServiceWorker();
