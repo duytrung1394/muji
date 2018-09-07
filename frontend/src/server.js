cbfc89acfb1920358492fc730e94bf05fea7ddb3
Satoshi SUZUKI <akn@s-satoshi.net>
2018-09-07 14:45:50 +0900

    開発中はwebpack-dev-server(webpack-dev-middleware)を使うようにした

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -5,14 +5,40 @@ import path from 'path';
 const app = express();
 const router = require('./serverRoutes');
 
-app.use(manifest({
-  manifest: path.join(__dirname, '../build/asset-manifest.json'),
-  prepend: path.join(__dirname, '../build'),
-  reqPathFind: /^(\/?)/,
-  reqPathReplace: '',
-  debug: true,
-}));
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  
+  const compiler = webpack({
+    entry: [
+      './src/index.js',
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [
+            {
+              loader: 'babel-loader',
+            }
+          ]
+        }
+      ]
+    },
+  });
 
+  app.use(webpackDevMiddleware(compiler, {
+    // options
+  }));
+}else{
+  app.use(manifest({
+    manifest: path.join(__dirname, '../build/asset-manifest.json'),
+    prepend: path.join(__dirname, '../build'),
+    reqPathFind: /^(\/?)/,
+    reqPathReplace: '',
+    debug: true,
+  }));
+}
 // app.use(express.static(path.join(__dirname, '../build')));
 
 app.use(router);
