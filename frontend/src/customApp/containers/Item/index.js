cc2a64aeb4b429b11e5aaa2dd59f72eb6f14e03a
RYO Murakami <moosan63@gmail.com>
2018-09-21 02:15:30 +0900

    一覧の見た目を整えた

--- frontend/src/customApp/containers/Item/index.js
+++ frontend/src/customApp/containers/Item/index.js
@@ -2,21 +2,44 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import actions from "../../redux/item/list/actions";
-import RestIndex from "../shared/index";
-import ItemSearch from "./search";
+import LayoutWrapper from "../../../components/utility/layoutWrapper";
+import PageHeader from "../../../components/utility/pageHeader";
+import IntlMessages from "../../../components/utility/intlMessages";
+import { Card, Row, Col } from "antd";
 
 class Index extends Component {
   render() {
     return (
-      <RestIndex
-        name="item"
-        pkName="item_code"
-        baseUrl="/dashboard/items"
-        columnNames={["item_code", "item_name"]}
-        linkColumn="item_code"
-        SearchComponent={ItemSearch}
-        {...this.props}
-      />
+      <LayoutWrapper>
+        <PageHeader>
+           Item Title
+        </PageHeader>
+        <div className="isoLayoutContent">
+        <Row>
+          <Col span={12}>
+            <h2>約120品目の家具の価格を見直しました。</h2>
+            <p>収納用品やユニットシェルフ、収納ベッドなどの価格を見直しました。生産工程を点検し、品質はそのままにさらにお求めやすい価格を実現しています</p>
+          </Col>
+          <Col span={12}><img src="https://dummyimage.com/630"/></Col>
+        </Row>
+        </div>
+        <div className="isoLayoutContent">
+          <Row>
+            {BEDS.map(bed =>(
+              <Col span={6}>
+                <Card
+                  title={bed.name}
+                  extra={<a href="#">商品詳細</a>}
+                  style={{ width: 300 }}
+                >
+                  <img src={bed.image_url}/>
+                  <p>税込み {bed.price} 円</p>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+      </LayoutWrapper>
     );
   }
 }
@@ -38,3 +61,26 @@ export default withRouter(
     }
   )(Index)
 );
+
+const BEDS = [
+  {
+    image_url: "https://dummyimage.com/250",
+    name: "収納ベッド・スモール・ウォールナット材",
+    price: "32,900"
+  },
+  {
+    image_url: "https://dummyimage.com/250",
+    name: "収納ベッド・シングル・ウォールナット材",
+    price: "34,900"
+  },
+  {
+    image_url: "https://dummyimage.com/250",
+    name: "収納ベッド・セミダブル・ウォールナット材",
+    price: "39,900"
+  },
+  {
+    image_url: "https://dummyimage.com/250",
+    name: "収納ベッド・ダブル・ウォールナット材",
+    price: "44,900"
+  }
+];
