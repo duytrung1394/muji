f779cb94bd8e2b8edf64d1ab4fd12bcc745b4897
Satoshi Suzuki <akn@s-satoshi.net>
2018-09-27 16:39:08 +0900

    home componentにreduxのtaskから取得するデータを入れた

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 import { Link } from 'react-router-dom';
 
-const Home = (props) => (
-  <div>
-    This Page is Home Page!! <br />
-    <Link to={'/about'}>about</Link>
-  </div>
+import actions from "./customApp/redux/task/list/actions";
+
+class Home extends Component {
+  componentDidMount() {
+    this.props.fetchRequest({page: 0, filters: '[]'});
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Task fetch result from Redux:</p>
+        <p>
+          { JSON.stringify(this.props.entities) }
+        </p>
+        <p>
+          fetching: { this.props.fetching }
+        </p>
+
+        This Page is Home Page!! <br />
+        <Link to={'/about'}>about</Link>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return state.Task.List.toJS();
+};
+
+const { fetch } = actions;
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    {
+      fetchRequest: fetch.request,
+    }
+  )(Home)
 );
 
-export default Home;
