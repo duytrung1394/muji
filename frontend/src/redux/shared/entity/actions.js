399aeec1bd0cadab69ad2a647e24fe4f3fb45fa7
Satoshi Suzuki <akn@s-satoshi.net>
2018-10-01 15:57:10 +0900

    customApp/redux/shared を redux 以下に復活

---
+++ frontend/src/redux/shared/entity/actions.js
@@ -0,0 +1,13 @@
+import req from "../requestActions";
+
+const restEntityActions = {
+  FETCH: req,
+  CREATE: req,
+  UPDATE: {
+    ...req,
+    REQUEST: (primaryKey, entity) => ({ primaryKey, entity })
+  },
+  DESTROY: req
+};
+
+export default restEntityActions;
---
+++ frontend/src/redux/shared/list/actions.js
@@ -0,0 +1,9 @@
+import req from "../requestActions";
+
+const restListActions = {
+  FETCH: req,
+  DESTROY: req,
+  SET_FILTERS: undefined
+};
+
+export default restListActions;
