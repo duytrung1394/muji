bd09d4c0505d735f4699e847bf0110b8ed66a6ec
ITO Kiyoharu <haruit@gmail.com>
2018-10-09 07:59:00 +0900

    [fix] TOPページ: 店舗情報: 都道府県の選択: 青森、沖縄に「県」を追加

--- frontend/src/customApp/components/form/selectPrefecture.js
+++ frontend/src/customApp/components/form/selectPrefecture.js
@@ -11,7 +11,7 @@ const SelectPrefecture = props => {
       >
       <Option value="00">都道府県から店舗を探す</Option>
       <Option value="北海道">北海道</Option>
-      <Option value="青森">青森</Option>
+      <Option value="青森県">青森県</Option>
       <Option value="岩手県">岩手県</Option>
       <Option value="宮城県">宮城県</Option>
       <Option value="秋田県">秋田県</Option>
@@ -56,7 +56,7 @@ const SelectPrefecture = props => {
       <Option value="大分県">大分県</Option>
       <Option value="宮崎県">宮崎県</Option>
       <Option value="鹿児島県">鹿児島県</Option>
-      <Option value="沖縄">沖縄</Option>
+      <Option value="沖縄県">沖縄県</Option>
     </Select>
   );
 };
