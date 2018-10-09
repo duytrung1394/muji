5073adff6ef817c0d83a95a14069c52749209cdf
山本佳史 <yamamoto@impl.co.jp>
2018-10-09 09:20:34 +0900

    [fix] :2489_notices修正

--- frontend/src/customApp/containers/Top/notice.js
+++ frontend/src/customApp/containers/Top/notice.js
@@ -6,15 +6,18 @@ const notice = props => {
   const { notices } = props;
   return (
     <div>
-      <p style={{marginLeft:'500px', marginRight:'600px'}}><IntlMessages id="top.attributes.notices"/>
+      <p style={{marginLeft:'500px', marginRight:'600px'}}>
+      <div  style={{border:'1px solid #dcdfe5', padding:'10px 50px'}}>
+      <IntlMessages id="top.attributes.notices"/>
         <ul>
           {
             notices && notices.map(notice => {
-              return <u><a href="https://www.muji.net/store/cmdty/donation/0299671000000"
+              return <u><a href="https://www.muji.net/store/cmdty/donation/0299671000000" style={{color:'grey'}}
               ><li>{notice.title}</li></a></u>
             })
           }
         </ul>
+        </div>
       </p>
     </div>
   );
