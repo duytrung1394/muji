7a7f383eb8f362d80e06bd88323cf5156ec0b546
kueba <koji.ueba@xenophy.com>
2018-10-12 22:16:25 +0900

    header調整

--- frontend/src/customApp/components/header/Header.js
+++ frontend/src/customApp/components/header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Layout, Form, Input, Select, Icon } from "antd";
+import { Layout, Form, Input, Select, Icon, Row, Col } from "antd";
 import HeaderWrapper from "./header.style";
 
 const AntdHeader = Layout.Header;
@@ -28,78 +28,56 @@ class Header extends Component {
 
     return (
       <HeaderWrapper>
-        <AntdHeader>
+        {bannerFlag === true ? (
           <div className="banner">
-            {bannerFlag === true ? (
-              <p>
-                <img
-                  src="https://img.muji.net/img/store/common/banner_ryohinweek.png"
-                  alt="無印良品週間 メンバー限定10％OFF"
-                />
-                <b>
-                  9/29(土) – 10/8(月)<span> ネットは10/9(火)午前10時まで</span>
-                </b>
-              </p>
-            ) : null}
-          </div>
-          <bannerFlagMng />
-          <div className="logo-search-icons">
-            <Link to="/store">
+            <p>
               <img
-                src="https://img.muji.net/img/common/logo-muji.svg"
-                width="128"
-                height="20"
-                alt="無印良品"
-                id="logo"
+                src="https://img.muji.net/img/store/common/banner_ryohinweek.png"
+                alt="無印良品週間 メンバー限定10％OFF"
               />
-            </Link>
-            <Form className="search">
-              <Input addonBefore={prefixSelector} />
-            </Form>
-            <ul>
-              <li>
-                <a class="cart">
-                  <Icon type="shopping-cart" style={{ fontSize: 32 }} />
-                  <p>カート</p>
-                </a>
-              </li>
-              <li class="login">
-                <a>
-                  <Icon type="user" style={{ fontSize: 32 }} />
-                  <p>ログイン</p>
-                </a>
-                <ul class="login-menu">
-                  <li>
-                    <a>ログイン</a>
-                  </li>
-                  <li>
-                    <a>新規登録</a>
-                  </li>
-                </ul>
-              </li>
-              <li class="support">
-                <a>
-                  <Icon type="info-circle" style={{ fontSize: 32 }} />
-                  <p>サポート</p>
-                </a>
-                <ul class="support-menu">
-                  <li>
-                    <a>お問い合わせ</a>
-                  </li>
-                  <li>
-                    <a>ご利用ガイド</a>
-                  </li>
-                  <li>
-                    <a>重要なお知らせ</a>
-                  </li>
-                  <li>
-                    <a>サイトからのお知らせ</a>
-                  </li>
-                </ul>
-              </li>
-              <li />
-            </ul>
+              <b>
+                9/29(土) – 10/8(月)<span> ネットは10/9(火)午前10時まで</span>
+              </b>
+            </p>
           </div>
+        ) : null}
+        <AntdHeader>
+          <Row type="flex" align="middle" justify="space-between" className="antd-header-row">
+            <Col className="header-logo">
+              <Link to="/store">
+                <img
+                  src="https://img.muji.net/img/common/logo-muji.svg"
+                  width="128"
+                  height="20"
+                  alt="無印良品"
+                  id="logo"
+                />
+              </Link>
+            </Col>
+            <Col className="header-search" >
+              <Form className="search">
+                <Input addonBefore={prefixSelector} />
+              </Form>
+            </Col>
+            <Col className="header-btns">
+              <Row type="flex" justify="space-between">
+                <Col className="header-btn">
+                  <a class="header-btn-anchor">
+                    <Icon type="shopping-cart" style={{ fontSize: 32 }} />
+                    <div>カート</div>
+                  </a>
+                </Col>
+                <Col className="header-btn">
+                  <Icon type="user" style={{ fontSize: 32 }} />
+                  <div>ログイン</div>
+                </Col>
+                <Col className="header-btn">
+                  <Icon type="info-circle" style={{ fontSize: 32 }} />
+                  <div>サポート</div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
         </AntdHeader>
       </HeaderWrapper>
     );
