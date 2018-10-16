4ac430bdc0d957669ecace1cd3e2ffdfceabad0f
YujinYano <>
2018-10-16 09:47:55 +0900

    [update] TOP画面: 毎日の無印良品/ネットストアの便利なサービス　eslint

--- frontend/src/customApp/components/top/dailyMuji.js
+++ frontend/src/customApp/components/top/dailyMuji.js
@@ -7,28 +7,36 @@ const DailyMuji = props => {
   const { dailyMujis } = props;
   return (
     <DailyMujiWrapper>
-        <div className="dailymuji-content-frame">
-          <a href="https://www.muji.net/store/cmdty/section/T001011" className="dailymuji-content-a">
-            <h2>
-              <IntlMessages id="top.dailyMuji.title" />
-            </h2>
-            <div className="dailymuji-item-group">
-              <ul>
-                {dailyMujis &&
-                  dailyMujis.map((item, i) => {
-                    return (
-                      <li key={i}>
-                        <img src={item.img_src} alt="imges" key={i} className="img" />
-                      </li>
-                    );
-                  })}
-              </ul>
-            </div>
-            <span className="dailyMuji-frame-subtitle">
-              <IntlMessages id="top.dailyMuji.subtitle" />
-            </span>
-          </a>
-        </div>
+      <div className="dailymuji-content-frame">
+        <a
+          href="https://www.muji.net/store/cmdty/section/T001011"
+          className="dailymuji-content-a"
+        >
+          <h2>
+            <IntlMessages id="top.dailyMuji.title" />
+          </h2>
+          <div className="dailymuji-item-group">
+            <ul>
+              {dailyMujis &&
+                dailyMujis.map((item, i) => {
+                  return (
+                    <li key={i}>
+                      <img
+                        src={item.img_src}
+                        alt="imges"
+                        key={i}
+                        className="img"
+                      />
+                    </li>
+                  );
+                })}
+            </ul>
+          </div>
+          <span className="dailyMuji-frame-subtitle">
+            <IntlMessages id="top.dailyMuji.subtitle" />
+          </span>
+        </a>
+      </div>
     </DailyMujiWrapper>
   );
 };
