0161b8af774f4557d7800f61514254ae386d2bc6
YujinYano <y.yano@impl.co.jp>
2018-10-22 17:36:10 +0900

    [add] TOP画面: from MUJI unsignedデータのrender()での対応 fromMuji.js

--- frontend/src/customApp/components/top/fromMuji.js
+++ frontend/src/customApp/components/top/fromMuji.js
@@ -1,7 +1,7 @@
 import IntlMessages from "../../../components/utility/intlMessages";
 import ContentPanel from "../panel/contentPanel";
 import ItemImageList from "../panel/itemImageList";
-import { Card,Button } from "antd";
+import { Card, Button } from "antd";
 import FromMujiWrapper from "./fromMuji.style";
 import React, { Component } from "react";
 import { connect } from "react-redux";
@@ -12,7 +12,6 @@ import { Row, Col } from "antd";
 const { Meta } = Card;
 
 class FromMuji extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -22,42 +21,54 @@ class FromMuji extends Component {
   }
 
   loadMore() {
-    this.setState((prev) => {
+    this.setState(prev => {
       return { visible: prev.visible + 10 };
     });
   }
 
   render() {
-  const { fromMujis } = this.props;
-  return (
-      <FromMujiWrapper>
-        <h1 className="from-muji-title">from MUJI</h1>
-        <Row type="flex" justify="center" align="top" style={{padding:0, margin:0}}>
+    const { fromMujis } = this.props;
+
+    if (fromMujis) {
+      return (
+        <FromMujiWrapper>
+          <h1 className="from-muji-title">from MUJI</h1>
+          <Row
+            type="flex"
+            justify="center"
+            align="top"
+            style={{ padding: 0, margin: 0 }}
+          >
             {fromMujis &&
-              fromMujis.slice(0, this.state.visible).map((item,index) => {
+              fromMujis.slice(0, this.state.visible).map((item, index) => {
                 return (
-                <Card
-                  hoverable
-                  cover={<img alt="example" src={item.img_src} />}
-                  className="cards"
-                  key={index}
-                  style={{margin:30, width:258 }}
-                >
-                 {index < 15 ? <span className="cards-new">NEW</span> : null}
-                  <Meta
-                    title={item.title}
-                    description={item.date}
-                    className="cards-meta"
-                  />
-                </Card>
+                  <Card
+                    hoverable
+                    cover={<img alt="example" src={item.img_src} />}
+                    className="cards"
+                    key={index}
+                    style={{ margin: 30, width: 258 }}
+                  >
+                    {index < 15 ? <span className="cards-new">NEW</span> : null}
+                    <Meta
+                      title={item.title}
+                      description={item.date}
+                      className="cards-meta"
+                    />
+                  </Card>
                 );
-             })}
-        </Row>
-        {this.state.visible < 45 &&
-          <p onClick={this.loadMore} type="button" className="load-more"><IntlMessages id="top.fromMuji.load_more" /></p>
-        }
-      </FromMujiWrapper>
-    );
+              })}
+          </Row>
+          {this.state.visible < Object.keys(fromMujis).length && (
+            <p onClick={this.loadMore} type="button" className="load-more">
+              <IntlMessages id="top.fromMuji.load_more" />
+            </p>
+          )}
+        </FromMujiWrapper>
+      );
+    } else {
+      return <FromMujiWrapper />;
+    }
   }
 }
 
