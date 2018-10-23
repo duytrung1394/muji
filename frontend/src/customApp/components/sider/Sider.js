d9c016bfcda4d767befb7ea52ab8e55590106558
kueba <koji.ueba@xenophy.com>
2018-10-23 18:36:37 +0900

    #2483 サイドメニュー: 全体的に調整

--- frontend/src/customApp/components/sider/Sider.js
+++ frontend/src/customApp/components/sider/Sider.js
@@ -4,62 +4,20 @@ import { Menu } from "antd";
 import {
   SiderWrapper,
   MenuItemGroup,
-  GroupMenuIcon,
+  MenuItemGroupIcon,
   MenuItem,
   LinkMenuItem
 } from "./sider.style";
 import NavLink from "./NavLink";
 import { Link } from "react-router-dom";
-
-import sidebarIcon2 from "./sider_icons/sidebarIcon2.png";
-import sidebarIcon3 from "./sider_icons/sidebarIcon3.png";
-import sidebarIcon4 from "./sider_icons/sidebarIcon4.png";
-import sidebarIcon5 from "./sider_icons/sidebarIcon5.png";
-import sidebarIcon6 from "./sider_icons/sidebarIcon6.png";
-import sidebarIcon7 from "./sider_icons/sidebarIcon7.png";
-import sidebarIcon8 from "./sider_icons/sidebarIcon8.png";
-import sidebarIcon9 from "./sider_icons/sidebarIcon9.png";
-import sidebarIcon10 from "./sider_icons/sidebarIcon10.png";
-import sidebarIcon11 from "./sider_icons/sidebarIcon11.png";
-import sidebarIcon12 from "./sider_icons/sidebarIcon12.png";
+import sidebarIcons from "./sider_icons/sidebarIcons"
 import menuJson from "./sidemenu.json";
 
-const menuData = menuJson.menus;
-
-const getSidebarIcon = img => {
-  switch (img) {
-    case "sidebarIcon2":
-      return sidebarIcon2;
-    case "sidebarIcon3":
-      return sidebarIcon3;
-    case "sidebarIcon4":
-      return sidebarIcon4;
-    case "sidebarIcon5":
-      return sidebarIcon5;
-    case "sidebarIcon6":
-      return sidebarIcon6;
-    case "sidebarIcon7":
-      return sidebarIcon7;
-    case "sidebarIcon8":
-      return sidebarIcon8;
-    case "sidebarIcon9":
-      return sidebarIcon9;
-    case "sidebarIcon10":
-      return sidebarIcon10;
-    case "sidebarIcon11":
-      return sidebarIcon11;
-    case "sidebarIcon12":
-      return sidebarIcon12;
-    default:
-      return null;
-  }
-};
-
-const SideMenuContents = ({ menus }) => {
+const SiderContents = () => {
   return (
     <Menu>
-      {menus &&
-        menus.map(menu => {
+      {menuJson.menus &&
+        menuJson.menus.map(menu => {
           switch (menu.type) {
             case "groupMenu":
               return (
@@ -67,7 +25,7 @@ const SideMenuContents = ({ menus }) => {
                   title={
                     <span>
                       {menu.img && (
-                        <GroupMenuIcon src={getSidebarIcon(menu.img)} />
+                        <MenuItemGroupIcon src={sidebarIcons[menu.img]} />
                       )}
                       <span>{menu.title}</span>
                     </span>
@@ -105,12 +63,12 @@ const SideMenuContents = ({ menus }) => {
   );
 };
 
-const SideMenu = () => {
+const Sider = () => {
   return (
     <SiderWrapper breakpoint="lg" collapsedWidth="0" width="230">
-      <SideMenuContents menus={menuData} />
+      <SiderContents />
     </SiderWrapper>
   );
 };
 
-export default SideMenu;
+export default Sider;
--- frontend/src/customApp/components/sider/sider.style.js
+++ frontend/src/customApp/components/sider/sider.style.js
@@ -2,38 +2,33 @@ import styled from "styled-components";
 import { Layout, Menu } from "antd";
 
 export const SiderWrapper = styled(Layout.Sider)`
-  .ant-layout-sider {
-    background: #fff;
-    border-right: solid 1px #f9f9f9;
-    position:left;
-    left: 0; 
-    background:#f7f7f7;
-  }
+  overflow-y: auto;
+  border-right: solid 1px #f9f9f9;
   
-  .ant-menu-item:hover{
+  .ant-menu-item:hover {
     color:white;
     background-color:grey;
-    a:hover{
-      display: block;
+
+    a:hover {
       color:white;
     }
   }
-`
+`;
+
 export const MenuItemGroup = styled(Menu.ItemGroup)`
   margin-bottom: 20px;
-  .ant-menu-item-group-title
-  {
-    border-top: 1px solid #ccc;
+  border-top: 1px solid #ccc;
+
+  .ant-menu-item-group-title {
     font-size:16px;
     color: #333;
     font-weight: 400;
   }  
 `;
 
-export const GroupMenuIcon = styled.img`
+export const MenuItemGroupIcon = styled.img`
   width:8%;
   height:15px;
-  display:inline;
   margin-right:5px;
   padding-bottom: 2px;
 `;
@@ -46,19 +41,22 @@ export const MenuItem = styled(Menu.Item)`
 `;
 
 export const LinkMenuItem = styled(Menu.Item)`
-  height: 70px !important;
+  height: 80px !important;
+  margin: 0 !important;
   font-weight: 700;
   font-family: Hiragino Kaku Gothic ProN;
-  padding: "0";
-  border-top:1px solid #ccc;
-  p {
-    font-size:8px;
-  }
+  border-top: 1px solid #ccc;
+
   a {
     padding-top:10px;
     line-height:28px;
-  }
-  span {
-    font-size:14px;
+
+    span {
+      font-size:14px;
+    }
+
+    p {
+      font-size:8px;
+    }
   }
 `;
