32f25d546a25cd81a6e98ccc0d7a511381615d8a
kueba <koji.ueba@xenophy.com>
2018-10-25 21:31:45 +0900

    lintignore追加，lint実行

--- frontend/src/customApp/components/top/netStore.js
+++ frontend/src/customApp/components/top/netStore.js
@@ -11,7 +11,7 @@ import {
 
 const services = [
   {
-    src: 'https://www.muji.com/jp/store/service/img/service_deliveryvan.png',
+    src: "https://www.muji.com/jp/store/service/img/service_deliveryvan.png",
     title: (
       <StyledServiceTitle>
         <span>一定額以上の</span>
@@ -19,10 +19,11 @@ const services = [
         <span className="emphasis">配送料無料</span>
       </StyledServiceTitle>
     ),
-    description: '小型商品は税込5,000円以上、大型家具・家電は税込150,000以上で、配送料無料',
+    description:
+      "小型商品は税込5,000円以上、大型家具・家電は税込150,000以上で、配送料無料"
   },
   {
-    src: 'https://www.muji.com/jp/store/service/img/service_storage.png',
+    src: "https://www.muji.com/jp/store/service/img/service_storage.png",
     title: (
       <StyledServiceTitle>
         <span>ポリプロピレン</span>
@@ -30,47 +31,48 @@ const services = [
         <span className="emphasis">配送料無料</span>
       </StyledServiceTitle>
     ),
-    description: '対象商品が配送料無料になりました',
+    description: "対象商品が配送料無料になりました"
   },
   {
-    src: 'https://www.muji.com/jp/store/service/img/service_deliveryvan02.png',
+    src: "https://www.muji.com/jp/store/service/img/service_deliveryvan02.png",
     title: (
       <StyledServiceTitle>
         <span className="emphasis">小型商品の</span>
         <span className="emphasis">配送料490円</span>
       </StyledServiceTitle>
     ),
-    description: '配送料区分Aの商品の配送料が490円になりました',
+    description: "配送料区分Aの商品の配送料が490円になりました"
   },
   {
-    src: 'https://www.muji.com/jp/store/service/img/service_box.png',
+    src: "https://www.muji.com/jp/store/service/img/service_box.png",
     title: (
       <StyledServiceTitle>
         <span className="emphasis">えらべる</span>
         <span className="emphasis">受取方法</span>
       </StyledServiceTitle>
     ),
-    description: 'ご自宅への配送以外にも、店舗受取や、コンビニ受取をご利用いただけます',
+    description:
+      "ご自宅への配送以外にも、店舗受取や、コンビニ受取をご利用いただけます"
   },
   {
-    src: 'https://www.muji.com/jp/store/service/img/service_shirt_s.png',
+    src: "https://www.muji.com/jp/store/service/img/service_shirt_s.png",
     title: (
       <StyledServiceTitle>
         <span className="emphasis">XS・XXL</span>
         <span className="emphasis">サイズ</span>
       </StyledServiceTitle>
     ),
-    description: '店舗からのお取り寄せや、店舗受取も可能です',
+    description: "店舗からのお取り寄せや、店舗受取も可能です"
   },
   {
-    src: 'https://www.muji.com/jp/store/service/img/service_hanger_s.png',
+    src: "https://www.muji.com/jp/store/service/img/service_hanger_s.png",
     title: (
       <StyledServiceTitle>
         <span className="emphasis">まとめ買い</span>
       </StyledServiceTitle>
     ),
-    description: 'ケースでお届けするから5%お得',
-  },
+    description: "ケースでお届けするから5%お得"
+  }
 ];
 
 const Title = ({ id }) => {
@@ -84,13 +86,10 @@ const Title = ({ id }) => {
 const Service = ({ service, key }) => {
   return (
     <StyledService key={key} span={4}>
-      <Card
-        cover={<img alt="" src={service.src} />}
-        style={{ border: "none" }}
-      >
+      <Card cover={<img alt="" src={service.src} />} style={{ border: "none" }}>
         <Card.Meta
           title={service.title}
-          description={(<p>{ service.description }</p>)}
+          description={<p>{service.description}</p>}
         />
       </Card>
     </StyledService>
@@ -102,17 +101,9 @@ const NetStore = () => {
     <NetStoreWrapper>
       <a href="">
         <Title id="top.net-store.title" />
-        <Row
-          type="flex"
-          justify="space-around"
-        >
+        <Row type="flex" justify="space-around">
           {services.map((service, index) => {
-            return (
-              <Service
-                key={index}
-                service={service}
-              />
-            );
+            return <Service key={index} service={service} />;
           })}
         </Row>
       </a>
