90dcd8b5aa9ae8143423188c9c4452da318f158d
山本佳史 <yamamoto@impl.co.jp>
2018-10-29 13:25:00 +0900

    [add] :2521_item_color_list  商品カラーリスト作成

--- frontend/src/customApp/components/productCategoryTop/itemList/itemSwatch.js
+++ frontend/src/customApp/components/productCategoryTop/itemList/itemSwatch.js
@@ -1,26 +1,48 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { Card } from "antd";
+import { Link } from 'react-router-dom';
+import { Menu, Dropdown, Icon } from 'antd';
 
 const Swatch = styled(Card.Grid)`
   width: 20% !important;
   box-shadow: none !important;
   padding: 0 !important;
   font-size: 9pt;
+  :hover{
+    cursor:pointer;
+  }
 `;
 
 const ImageWrapper = styled.div`
   cursor: pointer;
   width: 40px;
   height: 40px;
+  overflow:hidden;
+  border-radius:50%;
+
+
+  :hover{
+    border:2px solid grey;
+  }
+
+  img{
+    border-radius:50%;
+    padding:1px;
+    border:1px solid white;
+    position:
+  }
 `;
 
+
 class ItemSwatch extends Component {
+  
   state = {
     expanded: false
+    
   };
 
-  render() {
+  SwatchItem = () =>{
     const { swatches, changeSwatch } = this.props;
     const swatchLength = this.state.expanded
       ? swatches.length
@@ -28,6 +50,7 @@ class ItemSwatch extends Component {
         ? swatches.length
         : 4;
     let items = [];
+
     for (let i = 0; i < swatchLength; i++) {
       let jancode = swatches[i].jancode;
       let nostock = swatches[i].nostock;
@@ -44,11 +67,42 @@ class ItemSwatch extends Component {
     }
 
     return (
-      <div>
-        {items}
-        {!this.state.expanded && swatches.length > 4 && <Swatch>他10色</Swatch>}
+    <div>
+    {items}
+    </div>
+    );
+  }
+
+
+  render() {
+    const { swatches } =this.props;
+    return(
+      <div style={{justifyContent:"start", margin:"5px 0 4px",}}>
+        <this.SwatchItem />
+        <Link to={``}>
+        {this.items}
+        </Link>
+        {!this.state.expanded && swatches.length > 4 && 
+        <Swatch onClick={()=>{ this.state.expanded = true;}} >
+        他10色
+        <p>
+          <Icon type="down" theme="outlined" />
+        </p>
+        </Swatch>
+        }
+        {this.state.expanded &&
+        <Swatch onClick={()=>{ this.state.expanded = false}}>
+          <Icon type="up" theme="outlined" />
+          <p>
+          閉じる
+          </p>
+        </Swatch>
+        }
+        
       </div>
     );
+    
+
   }
 }
 
