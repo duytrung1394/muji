7cd4e54c2824d4c293c9b8588c665e5be269a3d7
kueba <koji.ueba@xenophy.com>
2018-10-29 22:00:13 +0900

    #2482 フッター調整

--- frontend/src/customApp/components/footer/Footer.js
+++ frontend/src/customApp/components/footer/Footer.js
@@ -1,83 +1,14 @@
 import React, { Component } from "react";
-import { Col } from "antd";
-import { StyledAntdFooter, NavRow, SiteMapRow, NavLink } from "./footer.style";
-
-import { IconList } from "./IconList";
-import { SiteMapList } from "./SiteMapList";
-
-const imgsData = [
-  "https://img.muji.net/img/common/twitter-with-circle.svg",
-  "https://img.muji.net/img/common/facebook-with-circle.svg",
-  "https://img.muji.net/img/common/youtube-with-circle.svg",
-  "https://img.muji.net/img/common/instagram-with-circle.svg"
-];
-
-const siteMapData = [
-  [
-    "店舗情報",
-    "イベント",
-    "くらしの良品研究所",
-    "NUJI SUPPORT",
-    "MUJI HOUSE VISION",
-    "お問い合わせ"
-  ],
-  [
-    "無印良品の家",
-    "Café&Meal MUJI",
-    "キャンプ場",
-    "Found MUJI",
-    "MUJI BOOKS",
-    "MUJI HOTEL"
-  ],
-  [
-    "MUJI passport",
-    "カタログ",
-    "MUJI Card",
-    "MUJI GIFT CARD",
-    "法人のお客様へ",
-    "Tax-free Services"
-  ],
-  [
-    "ニュースリリース",
-    "採用情報",
-    "無印良品メッセージ",
-    "無印良品について",
-    "株式会社　良品計画"
-  ]
-];
+import { Layout } from "antd";
+import { SiteMap } from "./SiteMap";
+import { Navigation } from "./Navigation";
 
 const Footer = () => {
   return (
-    <StyledAntdFooter>
-      <SiteMapRow>
-        <Col span={8}>
-          <img src="https://img.muji.net/img/common/logo-muji.svg" />
-          <IconList imgsData={imgsData} />
-        </Col>
-        {siteMapData &&
-          siteMapData.map((Data, index) => {
-            return (
-              <Col span={4} key={index}>
-                <SiteMapList Data={Data} />
-              </Col>
-            );
-          })}
-      </SiteMapRow>
-      <NavRow>
-        <Col span={2}>
-          <NavLink>日本</NavLink>
-        </Col>
-        <Col span={6}>
-          <NavLink>個人情報の取り扱い</NavLink>
-        </Col>
-        <Col span={4}>
-          <NavLink>サイトマップ</NavLink>
-        </Col>
-        <Col span={10} className="copy-right">
-          <p>Copyright ©Ryohin Keikaku Co., Ltd.</p>
-        </Col>
-      </NavRow>
-    </StyledAntdFooter>
+    <Layout.Footer>
+      <SiteMap />
+      <Navigation />
+    </Layout.Footer>
   );
 };
 
---
+++ frontend/src/customApp/components/footer/Icons.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const StyledUl = styled.ul`
+  display: flex;
+  padding: 0;
+  margin: 110px 0 0 0;
+
+  & > li {
+    list-style: none;
+    margin-right: 6px;
+
+    & > a {
+      opacity: 0.3;
+    }    
+  }
+`;
+
+const srcs = [
+  "https://img.muji.net/img/common/twitter-with-circle.svg",
+  "https://img.muji.net/img/common/facebook-with-circle.svg",
+  "https://img.muji.net/img/common/youtube-with-circle.svg",
+  "https://img.muji.net/img/common/instagram-with-circle.svg"
+];
+
+export const Icons = () => {
+  return (
+    <StyledUl>
+      {srcs.map((src, index) => {
+        return (
+          <li key={index}>
+            <a href="">
+              <img src={src} />
+            </a>
+          </li>
+        );
+      })}
+    </StyledUl>
+  );
+};
