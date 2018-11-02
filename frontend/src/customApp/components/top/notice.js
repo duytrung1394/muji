845e4a8c8b300ae6a65c8c3af4978d20dc2f27fd
山本佳史 <yamamoto@impl.co.jp>
2018-11-02 15:59:36 +0900

    [add] :2595_notice_refurbishment お知らせ改修作成

--- frontend/src/customApp/components/top/notice.js
+++ frontend/src/customApp/components/top/notice.js
@@ -1,26 +1,68 @@
 import React from "react";
 import IntlMessages from "../../../components/utility/intlMessages";
-import ContentLayout from "./notice.style";
+import { List } from "antd";
+import styled from "styled-components";
+
+const NoticeWrapper = styled(List)`
+text-align:center;
+font-weight:bold;
+*{
+  display:inline-block;
+  vertical-align:top; 
+}
+.ant-list-header{
+  padding:0;
+}
+.ant-list-bordered{
+  border-radius:6px;
+  max-width:1440px;
+  margin:0 auto;
+  padding:10px 20px;
+
+  .ant-list-header{
+    padding-right:0;
+  }
+
+  .ant-list-item{
+    padding-left:10px;
+  }
+}
+.ant-list-item{
+  padding:0;
+}
+a{
+  color:#888;
+  font-weight:normal;
+  text-decoration:underline;
+
+  :hover{
+    color:#555;
+    text-decoration:underline;
+  }
+}
+`;
 
 const notice = ({ notices }) => {
+  const data = notices &&
+    notices.map((notice, index) => {
+      return (
+        <div className="aaa">
+          <a  href="https://www.muji.net/store/cmdty/donation/0299671000000">
+            {notice.title}
+          </a>
+          </div>
+      );
+    })
+
   return (
-    <ContentLayout>
-      <dl>
-        <dt className="notice-title">
-          <IntlMessages id="top.attributes.notices" />
-        </dt>
-        {notices &&
-          notices.map((notice, index) => {
-            return (
-              <dd className="notice-list" key={index}>
-                <a href="https://www.muji.net/store/cmdty/donation/0299671000000">
-                  {notice.title}
-                </a>
-              </dd>
-            );
-          })}
-      </dl>
-    </ContentLayout>
+    <NoticeWrapper>
+      <List 
+        header={<IntlMessages id="top.attributes.notices" />}
+       dataSource={data}
+       renderItem={item => (<List.Item>{item}</List.Item>)}
+       bordered
+      />
+    </NoticeWrapper>
   );
 };
 
