0165d34ec2b9e55aaf644c8dcfee3eee8e5b2635
kusama <kusama@impl.co.jp>
2018-11-05 16:24:30 +0900

    [update] refs #2595 【EC】TOP画面: お知らせ改修 : 不要コード削除、スタイル設定調整
    ・コンポーネント内の不要<div>削除
    ・<NoticeWrapper>を<List>から<div>のoverrideに変更
    ・styled components を<NoticeWrapper>と<StyledList>に分割
    ・スタイル調整

--- frontend/src/customApp/components/top/notice.js
+++ frontend/src/customApp/components/top/notice.js
@@ -3,33 +3,37 @@ import IntlMessages from "../../../components/utility/intlMessages";
 import { List } from "antd";
 import styled from "styled-components";
 
-const NoticeWrapper = styled(List)`
+const NoticeWrapper = styled.div`
   text-align: center;
   font-weight: bold;
   * {
     display: inline-block;
-    vertical-align: top;
   }
-  .ant-list-header {
-    padding: 0;
-  }
-  .ant-list-bordered {
+`;
+
+const StyledList = styled(List)`
+  &.ant-list-bordered {
     border-radius: 6px;
-    max-width: 1440px;
     margin: 0 auto;
     padding: 10px 20px;
 
     .ant-list-header {
-      padding-right: 0;
+      vertical-align: top;
+      padding-top: 0px;
+      padding-left: 10px;
+      padding-right: 0px;
+      span {
+        width: 60px;
+      }
     }
 
     .ant-list-item {
+      padding: 0;
       padding-left: 10px;
+      border-bottom: 0px solid #e8e8e8;
     }
   }
-  .ant-list-item {
-    padding: 0;
-  }
+
   a {
     color: #888;
     font-weight: normal;
@@ -45,23 +49,22 @@ const NoticeWrapper = styled(List)`
 const notice = ({ notices }) => {
   const data =
     notices &&
-    notices.map((notice, index) => {
+    notices.map((notice) => {
       return (
-        <div className="aaa">
-          <a href="https://www.muji.net/store/cmdty/donation/0299671000000">
-            {notice.title}
-          </a>
-        </div>
+        <a href="https://www.muji.net/store/cmdty/donation/0299671000000">
+          {notice.title}
+        </a>
       );
     });
 
   return (
     <NoticeWrapper>
-      <List
+      <StyledList
         header={<IntlMessages id="top.attributes.notices" />}
         dataSource={data}
         renderItem={item => <List.Item>{item}</List.Item>}
-        bordered
+        bordered={true}
+        split={false}
       />
     </NoticeWrapper>
   );
