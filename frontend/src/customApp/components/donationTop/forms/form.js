0bb389cf0e598e21634fe17f5c48ef0dfc4d81b9
kusama <kusama@impl.co.jp>
2018-12-03 14:00:17 +0900

    [update] #2728 【EC】募金詳細 : lint実行、その他の募金リストの部分で修正したファイルのみ

--- frontend/src/customApp/components/donationTop/forms/form.js
+++ frontend/src/customApp/components/donationTop/forms/form.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
-import { Row, Col, Input, Form as AntdForm, Radio,Select,Button,Popover} from "antd";
+import {
+  Row,
+  Col,
+  Input,
+  Form as AntdForm,
+  Radio,
+  Select,
+  Button,
+  Popover
+} from "antd";
 import { Link } from "react-router-dom";
 import IntlMessages from "../../../../components/utility/intlMessages";
 import styled from "styled-components";
@@ -22,29 +31,26 @@ const formItemLayout = {
   }
 };
 
-const ImgCol = styled(Col)`
-
-`
+const ImgCol = styled(Col)``;
 const FormCol = styled(Col)`
-  width:45%;
-`
+  width: 45%;
+`;
 const SNSUl = styled.ul`
-  list-style:none;
-  display:flex;
+  list-style: none;
+  display: flex;
   justify-content: flex-end;
-`
+`;
 
 const TotalDonationWrapper = styled.div`
-  border-top:1px solid #ccc;
-  border-bottom:1px solid #ccc;
-  text-align:center;
-  margin-bottom:30px;
-`
+  border-top: 1px solid #ccc;
+  border-bottom: 1px solid #ccc;
+  text-align: center;
+  margin-bottom: 30px;
+`;
 
 class Form extends Component {
   state = {
-    entity: this.props.entity,
-
+    entity: this.props.entity
   };
 
   updateEntity = (keyName, value) => {
@@ -59,103 +65,101 @@ class Form extends Component {
     this.props.requestHandler(this.state.entity);
   };
 
-
   render() {
     const { entity } = this.props;
     return (
       <div>
         <AntdForm>
-          <Row type="flex" >
+          <Row type="flex">
             <ImgCol>
-            <img src={entity.img}/>
+              <img src={entity.img} />
             </ImgCol>
             <FormCol>
-              <h2>{entity.title}<br/>{entity.organization}</h2>
+              <h2>
+                {entity.title}
+                <br />
+                {entity.organization}
+              </h2>
               <p>{entity.reason}</p>
               <div>
-                <IntlMessages id="donationShow.note1"/><br/>
-                <IntlMessages id="donationShow.note2"/><br/>
-                <IntlMessages id="donationShow.note3"/>
+                <IntlMessages id="donationShow.note1" />
+                <br />
+                <IntlMessages id="donationShow.note2" />
+                <br />
+                <IntlMessages id="donationShow.note3" />
               </div>
-              <IntlMessages id="donationShow.unit"/>
+              <IntlMessages id="donationShow.unit" />
               10
-              <IntlMessages id="donationShow.yen"/>
+              <IntlMessages id="donationShow.yen" />
               <AntdForm.Item label="種類" colon={false} {...formItemLayout}>
                 <RadioGroup>
                   <RadioButton value="a">
-                    <IntlMessages id="donationShow.unit"/>
+                    <IntlMessages id="donationShow.unit" />
                     10
-                    <IntlMessages id="donationShow.yen"/>
+                    <IntlMessages id="donationShow.yen" />
                   </RadioButton>
                   <RadioButton value="b">
-                    <IntlMessages id="donationShow.unit"/>
+                    <IntlMessages id="donationShow.unit" />
                     100
-                    <IntlMessages id="donationShow.yen"/>
+                    <IntlMessages id="donationShow.yen" />
                   </RadioButton>
                 </RadioGroup>
                 <InputGroup compact>
-                  <IntlMessages id="donationShow.unitCount"/>
+                  <IntlMessages id="donationShow.unitCount" />
                   <Select defaultValue={1}>
                     <Option value={1}>1</Option>
                   </Select>
                 </InputGroup>
                 <Button type="primary">
-                  <IntlMessages id="donationShow.donate"/>
+                  <IntlMessages id="donationShow.donate" />
                 </Button>
               </AntdForm.Item>
               <Popover trigger="click">
                 <a>
-                  <IntlMessages id="donationShow.userGuide"/>
+                  <IntlMessages id="donationShow.userGuide" />
                 </a>
               </Popover>
               <Link to="/store/cmdty/donation/" key="01">
                 <Button>
-                  <IntlMessages id="donationShow.ticket"/>
+                  <IntlMessages id="donationShow.ticket" />
                 </Button>
               </Link>
-              </FormCol>
+            </FormCol>
           </Row>
           <SNSUl>
             <li>
               <Button type="primary" size="small" icon="twitter">
-                <IntlMessages id="donationShow.twitter"/>
+                <IntlMessages id="donationShow.twitter" />
               </Button>
             </li>
             <li>
               <Button type="primary" size="small" icon="like">
-                <IntlMessages id="donationShow.fb"/>
+                <IntlMessages id="donationShow.fb" />
               </Button>
             </li>
           </SNSUl>
           <TotalDonationWrapper>
-            <IntlMessages id="donationShow.total"/>
+            <IntlMessages id="donationShow.total" />
             <span>{entity.total}</span>
-            <IntlMessages id="donationShow.yen"/>
-            <IntlMessages id="donationShow.donationPeople"/>
+            <IntlMessages id="donationShow.yen" />
+            <IntlMessages id="donationShow.donationPeople" />
             <span>{entity.total_people}</span>
-            <IntlMessages id="donationShow.people"/>
-            <IntlMessages id="donationShow.current" values={{time : 16}} />
-            <div>
-              {entity.fundraising_description}
-            </div>
+            <IntlMessages id="donationShow.people" />
+            <IntlMessages id="donationShow.current" values={{ time: 16 }} />
+            <div>{entity.fundraising_description}</div>
           </TotalDonationWrapper>
           <Organization
-           title={entity.organization}
-           organizations={entity.organizations}
+            title={entity.organization}
+            organizations={entity.organizations}
           />
-          <CardList
-            items={entity.activities}
-            cardWidth={"25%"}
-          />
-          <Message
-          messages={entity.messages}
-          />
+          <CardList items={entity.activities} cardWidth={"25%"} />
+          <Message messages={entity.messages} />
           <CardList
             title={"その他の募金券"}
             items={entity.other_donations}
             cardWidth={"215px"}
           />
-          <Guide/>
+          <Guide />
           <Row>
             <Col span={12}>
               <Link to="/store/cmdty/donation">前のページに戻る</Link>
