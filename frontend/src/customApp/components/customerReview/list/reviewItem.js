fa492bda2e3a7913e757a556c083d5fe3c6a56a1
kusama <kusama@impl.co.jp>
2018-12-03 09:28:27 +0900

    [update] #2718 レビュー投稿履歴 : Linkタグを共通のものに差し替え

--- frontend/src/customApp/components/customerReview/list/reviewItem.js
+++ frontend/src/customApp/components/customerReview/list/reviewItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link } from "../../form/link";
 import { Icon, Rate, Popover } from "antd";
 
 const StyledItem = styled.li`
