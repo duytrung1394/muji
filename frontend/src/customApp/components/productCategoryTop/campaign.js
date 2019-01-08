bb268c0e7af51fbb83b94648589914a6f19897ae
ITO Kiyoharu <haruit@gmail.com>
2019-01-08 14:49:24 +0900

    [update]  #3046 商品一覧デザイン更新: 特集: アイテムの高さを揃える

--- frontend/src/customApp/components/productCategoryTop/campaign.js
+++ frontend/src/customApp/components/productCategoryTop/campaign.js
@@ -5,32 +5,40 @@ import styled from "styled-components";
 import settings from "../../../settings";
 import IntlMessages from "../../../components/utility/intlMessages";
 
-const StyledCol = styled(Col)`
-  text-align: center;
-  position: relative;
-  box-sizing: border-box;
-  margin: 0;
-  padding-bottom: 10px;
-
-  img {
-    width: 100%;
-  }
-
-  span {
-    display: block;
-    vertical-align: middle;
-    font-size: 13px;
-    word-wrap: break-word;
-    padding: 10px;
-    color: #000;
-  }
+const ItemImage = styled.img`
+  border-radius: 4px 4px 0 0;
+  width: 100%;
 `;
 
-const Box = styled.div`
-  border-radius: 4px;
+const ItemTitle = styled.div`
+  color: #585858;
+  text-align: center;
+  padding: 10px;
+`;
+
+const BoxShadow = styled.div`
   box-shadow: 0 1px 3px 0 #585858;
-  margin: 10px;
-  padding-bottom: 30px;
+  border-radius: 4px;
+  width: 100%;
+  margin-bottom: 10px;
+`;
+
+const colLayout = {
+  xs: 12,
+  sm: 12,
+  md: 6,
+  xl: 6
+};
+
+const rowGutter = {
+  xs: 16,
+  sm: 16,
+  md: 24,
+  xl: 32
+};
+
+const StyledCol = styled(Col)`
+  display: flex;
 `;
 
 const Campaign = ({ campaigns }) => {
@@ -42,33 +50,25 @@ const Campaign = ({ campaigns }) => {
       <h1>
         <IntlMessages id="productCategoryTop.title.campaign" />
       </h1>
-      <Row type="flex" justify="normal" align="top">
+      <Row type="flex" gutter={rowGutter}>
         {campaigns &&
           campaigns.map((item, index) => {
-            return <ItemIcon key={index} item={item} />;
+            return (
+              <StyledCol {...colLayout} key={index}>
+                <BoxShadow>
+                  <Link to="">
+                    <ItemImage
+                      src={`${settings.apiUrl}/${item.img_src}`}
+                      alt=""
+                    />
+                    <ItemTitle>{item.title}</ItemTitle>
+                  </Link>
+                </BoxShadow>
+              </StyledCol>
+            );
           })}
       </Row>
     </div>
-  );
-};
-
-const ItemIcon = props => {
-  const { item } = props;
-  const colLayout = {
-    xs: 12,
-    sm: 12,
-    md: 6,
-    xl: 6
-  };
-  return (
-    <StyledCol {...colLayout}>
-      <Link to={item.title}>
-        <Box>
-          <img src={`${settings.apiUrl}/${item.img_src}`} alt="" />
-          {item.title && <span>{item.title}</span>}
-        </Box>
-      </Link>
-    </StyledCol>
   );
 };
 
