2016d30d132ecfe24c90515e99e744debae3d347
kusama <kusama@impl.co.jp>
2019-01-16 21:55:53 +0900

    [update] refs #3111 商品詳細 : 商品イメージslick(途中)

--- frontend/src/customApp/components/productDetail/detailPicture.js
+++ frontend/src/customApp/components/productDetail/detailPicture.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import styled from "styled-components";
 import { Col, Modal, Button } from "antd";
 import fab from "../../../image/cmdty/detail/ico-fob-heart.png";
 import fabActive from "../../../image/cmdty/detail/ico-fob-heart-active.png";
+import Slider, { Link } from "../slider";
 
 const imgUrl = [
   "https://img.muji.net/img/item/4550002253899_1260.jpg",
@@ -13,11 +14,11 @@ const imgUrl = [
   "https://img.muji.net/img/item/4550002253899_05_1260.jpg"
 ];
 
-const PictureCol = styled(Col)`
-  max-height: 700px;
-  overflow: hidden;
-  position: relative;
-`;
+// const PictureCol = styled(Col)`
+//   max-height: 700px;
+//   overflow: hidden;
+//   position: relative;
+// `;
 
 const Img = styled.img`
   max-width: 100%;
@@ -60,6 +61,36 @@ const ShowItemList = styled.a`
   left: 10px;
 `;
 
+const ProductImageItemStyle = styled.div`
+  display: inline-block;
+  position: relative;
+  padding: 10px;
+  height: 375px;
+  width: 375px;
+`;
+
+const StyledLink = styled(Link)`
+  display: inline-block;
+  border-radius: 4px;
+  box-shadow: 0 1px 3px 0 #585858;
+
+  .product-image {
+    border-radius: 4px 4px 0 0;
+    width: 100%;
+  }
+`;
+
+
+const ProductImageItem = ({ productImage }) => {
+  return (
+    <ProductImageItemStyle>
+      <StyledLink to="">
+        <img className="product-image" src={productImage} />
+      </StyledLink>
+    </ProductImageItemStyle>
+  );
+};
+
 const FabImage = () => {
   let fabFlg = false;
   return (
@@ -95,11 +126,14 @@ class DetailPicture extends Component {
   };
 
   render() {
+    const customSliderSettings = {};
+    
     return (
-      <PictureCol span={12}>
-        {imgUrl.map((url, index) => {
+      <Fragment>
+      {/* <PictureCol span={24}> */}
+        {/* {imgUrl.map((url, index) => {
           return <Img key={index} src={url} />;
-        })}
+        })} */}
         <Dots>
           {imgUrl.map((url, index) => {
             return (
@@ -109,6 +143,11 @@ class DetailPicture extends Component {
             );
           })}
         </Dots>
+        <Slider {...customSliderSettings}>
+          {imgUrl.map((productImage, index) => {
+            return (<ProductImageItem key={index} productImage={productImage} />);
+          })}
+        </Slider>
         <ShowItemList onClick={this.showModal}>着目商品リスト</ShowItemList>
         <Modal
           title="Basic Modal"
@@ -117,7 +156,8 @@ class DetailPicture extends Component {
           onCancel={this.handleCancel}
         />
         <FabImage />
-      </PictureCol>
+      {/* </PictureCol> */}
+      </Fragment>
     );
   }
 }
