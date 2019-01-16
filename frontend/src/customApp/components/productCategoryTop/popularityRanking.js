e75e0f3fd736d7710631dcbfe1cb97420bf861c4
kueba <koji.ueba@xenophy.com>
2019-01-16 16:03:19 +0900

    #3046 ランキング側heightデザイン調整

--- frontend/src/customApp/components/productCategoryTop/popularityRanking.js
+++ frontend/src/customApp/components/productCategoryTop/popularityRanking.js
@@ -5,23 +5,45 @@ import IntlMessages from "../../../components/utility/intlMessages";
 import { BaseContentLayout } from "../../components/panel/contentLayout";
 import Slider from "../slider";
 
-const PopularityRankingTitle = styled.h1`
-  font-size: 25px;
-  padding: 10px;
-`;
-
 const StyledLink = styled(Link)`
-  position: relative;
-  display: block;
+  display: inline-block;
   border-radius: 4px;
   box-shadow: 0 1px 3px 0 #585858;
-  margin: 10px;
+
+  .ranking-item-image {
+    border-radius: 4px 4px 0 0;
+    width: 100%;
+  }
+
+  & > div {
+    padding: 10px;
+
+    .ranking-item-material {
+      color: #999;
+      font-size: 11px;
+      padding-bottom: 5px;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+      overflow: hidden;
+    }
+
+    .ranking-item-title {
+      min-height: 40px;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+      color: #000;
+      font-size: 12px;
+    }
+  }
 `;
 
 const Badge = styled.span`
   position: absolute;
-  left: 8px;
-  top: 8px;
+  left: 18px;
+  top: 18px;
   min-width: 16px;
   min-height: 12px;
   border-radius: 50%;
@@ -35,25 +57,33 @@ const Badge = styled.span`
   font-family: sans-serif;
 `;
 
-const ItemImage = styled.img`
-  border-radius: 4px 4px 0 0;
-  width: 100%;
+const RankingItemStyle = styled.div`
+  display: inline-block;
+  position: relative;
+  padding: 10px;
 `;
 
-const ItemMaterial = styled.div`
-  color: #999;
-  font-size: 11px;
-  padding: 5px 10px;
-`;
-const ItemTitle = styled.div`
-  color: #000;
-  font-size: 12px;
-  padding: 5px 10px 10px;
-`;
-
-const customSliderSettings = {
-  infinite: true
+const RankingItem = ({badgeNum, ranking}) => {
+  return (
+    <RankingItemStyle>
+      <StyledLink to="">
+        <img className="ranking-item-image" src={ranking.img} />
+        <div>
+          <div className="ranking-item-material">{ranking.material}</div>
+          <div className="ranking-item-title">{ranking.title}</div>
+        </div>
+      </StyledLink>
+      <Badge>{badgeNum}</Badge>
+    </RankingItemStyle>
+  );
 };
+
+const PopularityRankingTitle = styled.h1`
+  font-size: 25px;
+  padding: 10px;
+`;
+
+const customSliderSettings = {};
 
 const PopularityRanking = ({ rankings }) => {
   if (!rankings) {
@@ -67,16 +97,7 @@ const PopularityRanking = ({ rankings }) => {
       </PopularityRankingTitle>
       <Slider {...customSliderSettings}>
         {rankings.map((ranking, index) => {
-          return (
-            <div key={index}>
-              <StyledLink to="">
-                <ItemImage src={ranking.img} />
-                <Badge>{index + 1}</Badge>
-                <ItemMaterial>{ranking.material}</ItemMaterial>
-                <ItemTitle>{ranking.title}</ItemTitle>
-              </StyledLink>
-            </div>
-          );
+          return (<RankingItem key={index} badgeNum={index + 1} ranking={ranking} />);
         })}
       </Slider>
     </BaseContentLayout>
