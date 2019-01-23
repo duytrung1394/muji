c7361859a2056bd93ae58aee776f20e9f13ff386
kusama <kusama@impl.co.jp>
2019-01-23 19:11:39 +0900

    [update] refs #3111 商品詳細 : サンプルレスポンスデータ対応中

--- frontend/src/customApp/components/productDetail/detailInfo.js
+++ frontend/src/customApp/components/productDetail/detailInfo.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { Col } from "antd";
-import Detail from "./detail";
+import Price from "./price";
 import Color from "./color";
 import Size from "./size";
 import Quantity from "./quantity";
 import Buttons from "./buttons";
 import IntlMessages from "../../../components/utility/intlMessages";
+import { Link } from "../shared/form/link";
 
 const DetailName = styled.p`
   color: #999;
@@ -27,14 +28,39 @@ const DetailText = styled.p`
   font-size: 13px;
 `;
 
-const Info = ({ info }) => {
+const DetailList = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+const StoreJanCode = styled.li`
+  span {
+    font-size: 10px;
+  }
+`;
+const DistributionFee = styled.li`
+  margin-top : 5px;
+  span {
+    font-size: 15px;
+  }
+`;
+
+const LinkStyle = styled.span`
+  margin-left: 24px;
+  color: #60b3fa;
+  font-size: 13px;
+`;
+
+const Info = ({ description }) => {
+  const delimiter = "。";
+  const descriptionLines = description.split(delimiter);
   return (
     <div>
       <DetailText>
-        {info.messages.map((message, index) => {
+        {descriptionLines.map((line, index) => {
           return (
             <span key={index}>
-              {message}
+              {line}
+              {descriptionLines.length-1 > index && delimiter}
               <br />
             </span>
           );
@@ -52,7 +78,9 @@ const DetailInfo = ({ entity }) => {
     !entity.material ||
     !entity.read_more_info ||
     !entity.color_list ||
-    !entity.size_list
+    !entity.size_list ||
+    !entity.priceKindList || 
+    !entity.distribution_fee
   ) {
     return null;
   }
@@ -60,9 +88,27 @@ const DetailInfo = ({ entity }) => {
   return (
     <Col span={11} offset={1}>
       <DetailName>{entity.material}</DetailName>
-      <ItemName>{entity.title}</ItemName>
-      <Info info={entity.read_more_info} />
-      <Detail />
+      <ItemName>{entity.itemName}</ItemName>
+        <Info description={entity.webCatalogDescription} />
+      <DetailList>
+        <Price price={entity.price} priceKindList={entity.priceKindList}/>
+        <StoreJanCode>
+          <span>
+            <IntlMessages id="productDetail.itemNumber" />
+          </span>
+          <span> {entity.storeJanCode}</span>
+        </StoreJanCode>
+        <DistributionFee>
+          <IntlMessages id="productDetail.deliveryFee" />
+          <span> {entity.distribution_fee}</span>
+          <IntlMessages id="productDetail.yen" />
+          <LinkStyle>
+            <Link to="#">
+              <IntlMessages id="productDetail.detail" />
+            </Link>
+          </LinkStyle>
+        </DistributionFee>
+      </DetailList>
       <Color select={entity.color_list_select} colorList={entity.color_list} />
       <Size select={entity.size_list_select} sizeList={entity.size_list} />
       <Quantity />
---
+++ frontend/src/customApp/components/productDetail/price.js
@@ -0,0 +1,85 @@
+import React, { Fragment } from "react";
+import { Icon } from "antd";
+import IntlMessages from "../../../components/utility/intlMessages";
+import styled from "styled-components";
+import moment from "moment";
+
+const LargePrice = styled.span`
+  margin: 4px;
+  font-size: 14px;
+`;
+
+const Seal = styled.span`
+  font-size: 13px;
+  color: #7f0019;
+`;
+
+const SealPrice = styled.span`
+  font-size: 18px;
+  margin: 0 4px;
+`;
+
+const StyledIcon = styled(Icon)`
+  margin: 0 4px;
+`;
+
+const Unable = styled.div`
+  border: 1px solid #333;
+  font-size: 10px;
+  width: 100px;
+  padding: 0.5px 1px;
+  text-align: center;
+  margin: 8px 0;
+`;
+
+const Span = styled.span`
+  margin-top: 3px;
+  font-size: 10px;
+`;
+
+const Detail = ({ price, priceKindList }) => {
+  let startDate = null;
+  if (priceKindList.length > 0 && priceKindList[0].bannerInfo){
+    startDate = priceKindList[0].bannerInfo.startDate;
+  }
+  const startDateTime = startDate && startDate.length == 12 ? 
+    moment(startDate.substr(0, 8) + "T" + startDate.substr(8) ) : null;
+  return (
+    <Fragment>
+      <li>
+        <Unable>
+          <IntlMessages id="productDetail.unableToAcceptStore" />
+        </Unable>
+      </li>
+      {price.oldPrices ? 
+        <li>
+          <strike>
+            <Span>
+              <IntlMessages id="rest.taxIncluded" />
+              <LargePrice>{price.oldPrices[0].oldPrice.toLocaleString()}</LargePrice>
+              <IntlMessages id="productDetail.yen" />
+            </Span>
+          </strike>
+          <StyledIcon type="arrow-right" />
+        <Seal>
+          <IntlMessages id="rest.taxIncluded" />
+            <SealPrice>{price.basicPrice.toLocaleString()}</SealPrice>
+            <IntlMessages id="rest.priceCurrency" />
+        </Seal>
+        </li>
+        :
+        <li>
+          <IntlMessages id="rest.taxIncluded" />
+          <SealPrice>{price.basicPrice}</SealPrice>
+          <IntlMessages id="rest.priceCurrency" />
+        </li>
+      }
+      <li>
+        <Span>{startDateTime && startDateTime.format("YY/MM/DD")}</Span>
+        <Span> {startDateTime && startDateTime.format("AHH:mm")}</Span>
+      </li>
+    </Fragment>
+  );
+};
+
+export default Detail;
