4d98ac4419c1aa2fe0884afe6020c55b61dace5a
AsukaSuzuki <suzuki@impl.co.jp>
2019-01-28 17:07:44 +0900

    [update] refs #3068 【EC】EC-CO-001 注文手続き/通常 :  「ギフト包装」のviewを進めた

--- frontend/src/customApp/components/order/gift.js
+++ frontend/src/customApp/components/order/gift.js
@@ -2,6 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import IntlMessages from "../../../components/utility/intlMessages";
 
+const GiftWrapper = styled.div`
+  padding: 16px;
+  margin-top: 30px;
+  background-color: #F2F2F2;
+`;
+
+const GiftStyle = styled.div`
+  padding: 16px;
+  background-color: #FFF;
+`;
+
 const Title = styled.h1`
   font-size: 15px;
   font-weight: 600;
@@ -10,15 +21,17 @@ const Title = styled.h1`
 
 const Gift = () => {
   return (
-    <div>
-      <Title>
-        ギフト包装
-      </Title>
-      ギフト包装を利用する（162円）
-      まとめて包装する
-      のし：指定なし　メッセージ：指定なし
-      のし・メッセージの指定
-    </div>
+    <GiftWrapper>
+      <GiftStyle>
+        <Title>
+          ギフト包装
+        </Title>
+        ギフト包装を利用する（162円）<p />
+        まとめて包装する<p />
+        のし：指定なし　メッセージ：指定なし<p />
+        のし・メッセージの指定
+      </GiftStyle>
+    </GiftWrapper>
   );
 };
   
