018cb21f0515c1232ca88dcb1c250ca7c3f95b2c
ITO Kiyoharu <haruit@gmail.com>
2019-02-06 09:24:47 +0900

    [update] #3141 EC-PL-001　商品一覧 ver1.2: 商品一覧グループ名右端の左右スライド用リンク追加

--- frontend/src/customApp/components/productCategoryTop/itemList/itemList.js
+++ frontend/src/customApp/components/productCategoryTop/itemList/itemList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import ContentPanel from "../../shared/panel/contentPanel";
 import LargeButton from "../../shared/form/largeButton";
@@ -6,7 +6,9 @@ import ItemListHeader from "./itemListHeader";
 import ItemView from "./itemView";
 import IntlMessages from "../../../../components/utility/intlMessages";
 import CategoriesInPage from "./categoriesInPage";
-import Slider from "../../shared/slider";
+import { defaultSettings } from "../../shared/slider";
+import Slider from "react-slick";
+import eventLink from "../../../../image/event/ico-event-link.png";
 
 const ContentPanelWrapper = styled(ContentPanel)`
   .ant-card-head {
@@ -25,11 +27,33 @@ const ContentPanelWrapper = styled(ContentPanel)`
       max-width: 500px;
     }
   }
+  .slick-track {
+    display: flex;
+  }
+  .slick-slide {
+    height: inherit;
+  }
 `;
 
-const GroupName = styled.h1`
-  font-size: 19px;
-  font-weight: bold;
+const GroupHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  h1 {
+    font-size: 19px;
+    font-weight: bold;
+  }
+  .next,
+  .previous {
+    height: 32px;
+  }
+  .previous {
+    cursor: pointer;
+    transform: scale(-1, 1);
+  }
+  .next {
+    cursor: pointer;
+    margin-left: 40px;
+  }
 `;
 
 const StyledLargeButton = styled(LargeButton)`
@@ -72,39 +96,73 @@ const ItemCountButton = props => (
   </StyledLargeButton>
 );
 
-const ItemList = props => {
-  const { total, groups, categories_in_page } = props;
+class ItemList extends Component {
+  state = {
+    slider: []
+  };
 
-  return (
-    <ContentPanelWrapper
-      extra={<ItemListHeader total={total} />}
-      actions={categories_in_page ? [] : [<SeeMoreButton />]}
-    >
-      {!categories_in_page &&
-        groups &&
-        groups.map((group, index) => {
-          return (
-            <div key={index}>
-              <GroupName>{group.group_name}</GroupName>
-              <Slider>
-                {group.items.map((item, index) => {
-                  return (
-                    <ItemView isSlideScroll={true} {...item} key={index} />
-                  );
-                })}
-              </Slider>
-              <ItemCountButtonWrapper>
-                <ItemCountButton name={group.group_name} count={group.total} />
-              </ItemCountButtonWrapper>
-            </div>
-          );
-        })}
+  next = index => {
+    this.state.slider[index].slickNext();
+  };
 
-      {categories_in_page && (
-        <CategoriesInPage categories={categories_in_page} groups={groups} />
-      )}
-    </ContentPanelWrapper>
-  );
-};
+  previous = index => {
+    this.state.slider[index].slickPrev();
+  };
+
+  render() {
+    const { total, groups, categories_in_page } = this.props;
+
+    return (
+      <ContentPanelWrapper
+        extra={<ItemListHeader total={total} />}
+        actions={categories_in_page ? [] : [<SeeMoreButton />]}
+      >
+        {!categories_in_page &&
+          groups &&
+          groups.map((group, index) => {
+            return (
+              <div key={index}>
+                <GroupHeader>
+                  <h1>{group.group_name}</h1>
+                  <div>
+                    <img
+                      className="previous"
+                      src={eventLink}
+                      onClick={() => this.previous(index)}
+                    />
+                    <img
+                      className="next"
+                      src={eventLink}
+                      onClick={() => this.next(index)}
+                    />
+                  </div>
+                </GroupHeader>
+                <Slider
+                  {...defaultSettings}
+                  ref={slider => (this.state.slider[index] = slider)}
+                >
+                  {group.items.map((item, index) => {
+                    return (
+                      <ItemView isSlideScroll={true} {...item} key={index} />
+                    );
+                  })}
+                </Slider>
+                <ItemCountButtonWrapper>
+                  <ItemCountButton
+                    name={group.group_name}
+                    count={group.total}
+                  />
+                </ItemCountButtonWrapper>
+              </div>
+            );
+          })}
+
+        {categories_in_page && (
+          <CategoriesInPage categories={categories_in_page} groups={groups} />
+        )}
+      </ContentPanelWrapper>
+    );
+  }
+}
 
 export default ItemList;
--- frontend/src/customApp/components/shared/slider/index.js
+++ frontend/src/customApp/components/shared/slider/index.js
@@ -63,4 +63,4 @@ const Slider = props => {
 
 export default Slider;
 
-export { Link };
+export { defaultSettings, Link };
