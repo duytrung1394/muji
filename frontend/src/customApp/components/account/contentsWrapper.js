59afa6854037cc61d4bb4aa40fbb79793efa5851
kuramoto <kuramoto@impl.co.jp>
2019-02-22 11:41:19 +0900

    #3526_un_rg_001  修正と共通ボタンの作成

--- frontend/src/customApp/components/account/contentsWrapper.js
+++ frontend/src/customApp/components/account/contentsWrapper.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
 import styled from "styled-components";
-import { Link } from "../../components/shared/form/link";
+import { Link } from "../shared/form/link";
 import IntlMessages from "../../../components/utility/intlMessages";
+import CommonButton from "./button";
 
-const MemberGuidHeadTitle = styled.h1`
+const MemberGuideHeadTitle = styled.h1`
   color: #000;
   font-size: 19px;
   letter-spacing: 0.25px;
@@ -18,32 +19,11 @@ const MemberGuideHeadText = styled.p`
   margin: 20px 0 0;
 `;
 
-const Button = styled.button`
-  background: #7f0019;
-  border: 1px solid #7f0019;
-  border-radius: 20px;
-  box-shadow: 0 1px 3px rgba(88, 88, 88, 0.3);
-  cursor: pointer;
-  display: block;
-  font-size: 12px;
-  height: 40px;
-  margin: 20px auto 0;
-  max-width: 300px;
-  text-align: center;
-  width: 100%;
-`;
-
-const ToMembershipAgreement = styled(Link)`
-  color: #fff;
-`;
-
 const MemberRegistrationButton = () => {
   return (
-    <Button>
-      <ToMembershipAgreement to="#">
-        <IntlMessages id="account.memberRegistration" />
-      </ToMembershipAgreement>
-    </Button>
+    <CommonButton>
+      <IntlMessages id="account.memberRegistration" />
+    </CommonButton>
   );
 };
 
@@ -51,9 +31,9 @@ const ContentsWrapper = props => {
   return (
     <Fragment>
       <div>
-        <MemberGuidHeadTitle>
+        <MemberGuideHeadTitle>
           <IntlMessages id="account.memberGuideHeadTitle" />
-        </MemberGuidHeadTitle>
+        </MemberGuideHeadTitle>
         <MemberGuideHeadText>
           <IntlMessages id="account.memberGuideHeadText" />
         </MemberGuideHeadText>
