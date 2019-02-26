9ad641c170f8d0367400e4459d0f742048126c34
Satoshi Suzuki <akn@s-satoshi.net>
2019-02-26 19:55:18 +0900

    BASIC認証に用いる環境変数を整理

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,10 +1,10 @@
 #/bin/bash -eu
 
-envsubst '$$HOST $$EN_SSL $$DIS_SSL $$DIS_BASIC' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
 if [[ -z "${BASIC_USER}" ]]; then
+    export DIS_BASIC="#"
 	echo "Skip htpasswd generation."
 else
+    export DIS_BASIC=
 	htpasswd -c -b /etc/nginx/.htpasswd $BASIC_USER $BASIC_PASS
     echo "/etc/nginx/.htpasswd is created successfully"
     cat /etc/nginx/.htpasswd
@@ -18,5 +18,6 @@ else
     export DIS_SSL=
 fi
 
+envsubst '$$HOST $$EN_SSL $$DIS_SSL $$DIS_BASIC' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
