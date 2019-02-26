19dc4f0b9d0a23ab9eb8edd6df38f43ee6060544
kusama <kusama@impl.co.jp>
2019-02-26 17:22:35 +0900

    [update] refs #3068,3069 EC-CO-001,002 注文手続き : 入力/確認画面で重複していたコンポーネントファイルを削除

--- frontend/src/customApp/containers/Order/index.js
+++ frontend/src/customApp/containers/Order/index.js
@@ -25,7 +25,6 @@ class Index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedKeys: [],
       paymentOptions: {
         shoppingPoints: {
           value: 1,
