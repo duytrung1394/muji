f81985c172b1de6eb8304df2a808f76d2a17523e
Satoshi Suzuki <akn@s-satoshi.net>
2019-02-26 20:07:10 +0900

    yesno導入とデプロイスクリプトの調整

--- shipitfile.js
+++ shipitfile.js
@@ -1,3 +1,4 @@
+const yesno = require('yesno')
 
 module.exports = shipit => {
   shipit.initConfig({
@@ -28,13 +29,23 @@ module.exports = shipit => {
       branch,
     } = shipit.config;
 
+    await shipit.local(`git fetch`)
+    const localRev = await shipit.local(`git rev-parse ${branch}`)
+    const originRev = await shipit.local(`git rev-parse origin/${branch}`)
+    if( localRev.stdout !== originRev.stdout ){
+      const result = await yesno.askAsync('ローカルの最新コミットがGitの最新版と異なるようです。 デプロイをこのまま実行しますか？', true);
+      if(result === false){
+        throw "Noが選択されました";
+      }
+    }
+
     const r = async (cmd) => {
       // cwd(カレントディレクトリ)オプションを常に渡すために関数で包む
       await shipit.remote(cmd, {cwd})
     }
 
-    r(`git checkout ${branch}`)
-    r('git pull')
+    await r(`git checkout ${branch}`)
+    await r('git pull')
     
     // docker-compose での実行をつつむ
     const dc = async (dc_cmd) => {
@@ -47,10 +58,10 @@ module.exports = shipit => {
       }
     }
 
-    dc('build')
+    await dc('build')
 
     console.log('backend 環境整備開始')
-    runComposeCommands([
+    await runComposeCommands([
       'run --rm backend composer install',
       'run --rm backend php artisan migrate:refresh',
       'run --rm backend composer dump-autoload',
@@ -60,7 +71,7 @@ module.exports = shipit => {
     console.log('backend 立ち上げ完了')
 
     console.log('frontend 環境整備開始')
-    runComposeCommands([
+    await runComposeCommands([
       'run --rm frontend yarn install',
       'run --rm frontend yarn build',
       'up -d frontend',
@@ -68,7 +79,7 @@ module.exports = shipit => {
     console.log('frontend 立ち上げ完了')
 
     console.log('nginx 立ち上げ開始')
-    runComposeCommands([
+    await runComposeCommands([
       'up -d nginx',
     ])
     console.log('nginx 立ち上げ完了')
